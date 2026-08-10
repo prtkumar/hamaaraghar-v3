160182e34c9042007326e0bd282259a2d59200bb
prtkumar <prabhatkumar286266@gmail.com>
2026-08-10 12:12:08 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
+/* ===========================
+   HAMAARA GHAR V6
+=========================== */
 
+const gate = document.getElementById("gate");
+const hero = document.getElementById("hero");
+
+let opened = false;
+
+function enterHome(){
+
+    if(opened) return;
+
+    opened = true;
+
+    document.body.classList.add("open");
+
+    /* 2.5 सेकण्ड बाद Gate हटे */
+
+    setTimeout(()=>{
+
+        gate.style.display="none";
+
+    },2500);
+
+}
+
+gate.addEventListener("click",enterHome);
+
+document.addEventListener("keydown",(e)=>{
+
+    if(e.code==="Space"){
+
+        enterHome();
+
+    }
+
+});
